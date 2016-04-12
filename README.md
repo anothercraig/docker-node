7e67cbd198dbb87a15f7e651ad2d2bfe4a283fd4
craigr <craigr@pageuppeople.com>
2016-04-12 16:36:13 +1000

    minor changes

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ NODE SPIKE
 `docker build -t node-spike .`
 
 `docker run -it -p 8888:8888 node-spike`
+
+To run the container in the background...
+`docker run -d -p 8888:8888 node-spike`
--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t node-spike .
-docker run -it -v "$DIR/src":/opt/node-spike node-spike bash
+# to mount the local dir
+# docker run -it -v "$DIR/src":/opt/node-spike node-spike bash
+docker run -d -p 8888:8888 node-spike
